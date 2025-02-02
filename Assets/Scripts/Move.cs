a815efac2325333df9acd20461bde4018e5fb070
Simona <sima.simute1999@gmail.com>
2025-02-02 15:24:14 +0200

    movement

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public LayerMask groundLayer;
+    public Transform feet;
+    public float speed = 10;
+    public float gravity = -9.8f;
+    public float jumpHeight = 2;
+    
+    private CharacterController controller;
+    private float y;
+    private bool isGrounded;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(feet.position, 0.4f, groundLayer);
+
+        if (isGrounded) y = 0;
+        
+        var x = Input.GetAxisRaw("Horizontal");
+        var z = Input.GetAxisRaw("Vertical");
+        
+        var move = (transform.right * x + transform.forward * z) * speed * Time.deltaTime;
+        
+        //jump
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            y = Mathf.Sqrt(jumpHeight * -2f * gravity) * Time.deltaTime;
+        }
+        
+        //add gravity
+        y += gravity * Time.deltaTime * Time.deltaTime;
+        move.y = y;
+        
+        controller.Move(move);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (feet != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(feet.position, 0.4f);
+        }
+    }
+}
